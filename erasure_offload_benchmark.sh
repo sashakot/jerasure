6bec7d6f2482a1d0fdf593de53eeab07fd8a3664
Sasha Kotchubievsky <sashakot@mellanox.com>
2017-11-20 08:12:53 +0000

    erasure_offload_benchmark.sh: Add checking for k and m and run with/without offload
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>

--- erasure_offload_benchmark.sh
+++ erasure_offload_benchmark.sh
@@ -13,14 +13,15 @@ PIDSTAT=${PIDSTAT:-"/hpc/local/work/sashakot/sysstat/pidstat"}
 # Runtime parameters
 #
 
-iter=${iter:-1000000}
+iter=${iter:-1}
 k=${k:-2}
 m=${m:-1}
 min_kb=${min_kb:-102}
-max_kb=${max_kb:-500}
+max_kb=${max_kb:-102}
 step_kb=${step_kb:-2}
 core=${core:-$[$core_num/2]}
 tmpfile=$(mktemp /tmp/erasure-benchmark-script.XXXXXX)
+ib_dev=${ib_dev:-"mlx5_2"}
 
 echo "Configuration:"
 echo $CEPH_BIN
@@ -29,10 +30,22 @@ echo $CEPH_ERASURE_BENCHMARK
 echo ""
 echo "Runtime parameters"
 echo "Iterations: $iter"
+
 echo "k: $k"
+
+if [[ (( $k != 2 )) ]]; then
+	echo "WARNING: k > 2 is not supported. Mememory registarion problem"
+fi
+
 echo "m: $m"
+
+if [[ (( $m != 1 )) ]]; then
+	echo "WARNING: m > 1 is not supported. Mememory registarion problem"
+fi
+
 echo "min data size in: $min_kb (KB)"
 echo "max data size in: $max_kb (KB)"
+echo "ib device: $ib_dev"
 echo "core $core"
 
 taskset="taskset -c $core "
@@ -45,17 +58,31 @@ function clean
 
 trap clean EXIT
 
+export MLNX_K=$k
+export MLNX_M=$m
+export MLNX_IB_DEV=$ib_dev
+
+function run_test()
+{
+	local bytes=$1
+	local iter=$2
+	local offload=$3
+
+	params="-w encode  -P k=$k -P m=$m -P technique=reed_sol_van -s $bytes -i $iter"
+	MLNX_OFFLOAD=$offload ${pidstat}  ${taskset} ${CEPH_ERASURE_BENCHMARK} ${params} > ${tmpfile}
+
+	reported_time=$(cat ${tmpfile} | awk ' NF==2 {print $1} ')
+	cpu=$(grep Average ${tmpfile} | awk '{print $8}')
+
+	printf '%.2f,\t\t%.0f\n' $reported_time $cpu
+}
+
 echo ""
 printf "%s,\t%s,\t%s\n" "Size (KB)" "Time (sec)" "CPU (%)"
 for kb_bytes in $(seq $min_kb $step_kb $max_kb); do
 	rm -f ${tmpfile}
 
 	bytes=$[$kb_bytes*1024]
-	params="-w encode  -P k=$k -P m=$m -P technique=reed_sol_van -s $bytes -i $iter"
-	${pidstat}  ${taskset} ${CEPH_ERASURE_BENCHMARK} ${params} > ${tmpfile}
 
-	reported_time=$(cat ${tmpfile} | awk ' NF==2 {print $1} ')
-	cpu=$(grep Average ${tmpfile} | awk '{print $8}')
-
-	printf '%d,\t\t%.2f,\t\t%.0f\n' $kb_bytes $reported_time $cpu
+	printf '%d,\t%s,\t%s\n' $kb_bytes $(run_test $bytes $iter OFF) $(run_test $bytes $iter ON)
 done
